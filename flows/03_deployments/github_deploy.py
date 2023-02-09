1e3fa43fb43a936372b24db5cc5e8c29141b9ce6
Ahmad Ammari <ammariect@gmail.com>
2023-02-10 01:24:41 +1100

    created to address w2 q4

--- flows/03_deployments/github_deploy.py
+++ flows/03_deployments/github_deploy.py
@@ -1,22 +1,16 @@
-import prefect
-from prefect import Flow, Task
-from prefect.storage import GitHub
+from prefect.deployments import Deployment
+from parameterized_flow_q4 import etl_parent_flow
+from prefect.filesystems import GitHub
 
-# Create a Task to represent your Prefect flow
-class MyFlowTask(Task):
-    def run(self):
-        # Import the flow from your "parameterized_flow_q4.py" script
-        from parameterized_flow_q4 import etl_parent_flow
-        # Run the flow
-        etl_parent_flow.run()
+github_block = GitHub.load("github-zoom")
+
+github_dep = Deployment.build_from_flow(
+    flow=etl_parent_flow,
+    name="github-flow",
+    storage=github_block,
+    entrypoint="flows\03_deployments\parameterized_flow_q4.py"
+)
 
 
-# Initialize a Flow object
-flow = Flow("My Prefect Flow", storage=GitHub(repo="https://github.com/anammari/prefect-zoomcamp"))
-
-# Add the MyFlowTask to the flow
-task = MyFlowTask()
-flow.add_task(task)
-
-# Register the flow with Prefect Cloud
-flow.run()
+if __name__ == "__main__":
+    github_dep.apply()
