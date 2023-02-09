5f5c5fedd967880f1ee93c0d32b9ec073ff30df4
Ahmad Ammari <ammariect@gmail.com>
2023-02-10 00:23:51 +1100

    created to address w2 q4

--- flows/03_deployments/github_deploy.py
+++ flows/03_deployments/github_deploy.py
@@ -1,23 +1,22 @@
+import prefect
 from prefect import Flow, Task
-from prefect.engine.executors import LocalDaskExecutor
 from prefect.storage import GitHub
-
-# Import your Prefect flow
-from parameterized_flow_q4 import etl_parent_flow
 
 # Create a Task to represent your Prefect flow
 class MyFlowTask(Task):
-    def run(self, flow_reference: str = "flows/03_deployments/parameterized_flow_q4.etl_parent_flow"):
-        # Load the flow from your forked GitHub repository
-        with open(flow_reference, "r") as file:
-            exec(file.read(), globals())
-        
-        # Return the flow object
-        return eval(flow_reference)
+    def run(self):
+        # Import the flow from your "parameterized_flow_q4.py" script
+        from parameterized_flow_q4 import etl_parent_flow
+        # Run the flow
+        etl_parent_flow.run()
 
-# Create the Prefect flow
-with Flow("My Prefect Flow", storage=GitHub(repo="anammari/prefect-zoomcamp")) as flow:
-    task = MyFlowTask()
 
-# Deploy the Prefect flow
-flow.run(executor=LocalDaskExecutor())
+# Initialize a Flow object
+flow = Flow("My Prefect Flow", storage=GitHub(repo="anammari/prefect-zoomcamp"))
+
+# Add the MyFlowTask to the flow
+task = MyFlowTask()
+flow.add_task(task)
+
+# Register the flow with Prefect Cloud
+flow.run()
